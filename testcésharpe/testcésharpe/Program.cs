7bf44390ea10530c8dd1317f22b5a5e149c96e4d
shaossword <45566987+shaossword@users.noreply.github.com>
2020-09-07 23:02:22 +0200

    avancer

--- testcésharpe/testcésharpe/Program.cs
+++ testcésharpe/testcésharpe/Program.cs
@@ -8,14 +8,17 @@ namespace testcésharpe
         {
             Console.WriteLine("Bonjour veuillez choisir un nombre.");
             Boolean flag = false;
-            while(flag == false){
             string saisieUn = Console.ReadLine();
-            Console.WriteLine("Vous avez saisi : " + saisieUn + " etes vous d'accord ? ");
+            while (flag == false)
+            {
+                Console.WriteLine("Vous avez saisi : " + saisieUn + " etes vous d'accord ? ");
                 Console.WriteLine("Veuillez ecrire oui ou non.");
                 string valide = Console.ReadLine();
-                if(valide == "oui") {
+                if (valide == "oui")
+                {
                     flag = true;
-                }else if(valide == "non")
+                }
+                else if (valide == "non")
                 {
                     Console.WriteLine("Choisissez un autre nombre.");
                     flag = false;
@@ -27,7 +30,56 @@ namespace testcésharpe
                 }
 
             }
-            //Console.ReadLine();
+            Console.WriteLine("Veuillez choisir un deuxieme nombre.");
+            string saisieDeux = Console.ReadLine();
+            while (flag == false)
+            {
+                Console.WriteLine("Vous avez saisi : " + saisieDeux + " etes vous d'accord ? ");
+                Console.WriteLine("Veuillez ecrire oui ou non.");
+                string valideDeux = Console.ReadLine();
+                if (valideDeux == "oui")
+                {
+                    flag = true;
+                }
+                else if (valideDeux == "non")
+                {
+                    Console.WriteLine("Choisissez un autre nombre.");
+                    flag = false;
+                }
+                else
+                {
+                    Console.WriteLine("Votre entrer est incorrect recommancer.");
+                    flag = false;
+                }
+
+            }
+            Console.WriteLine("Veuillez choisir une operation.");
+            string operateur = Console.ReadLine();
+            int un = (int)Int64.Parse(saisieUn);
+            int deux = (int)Int64.Parse(saisieDeux);
+            int result;
+            switch (operateur)
+            {
+                case "+":
+                    result = un + deux;
+                    Console.WriteLine("Resultat " + result);
+                    break;
+                case "-":
+                    result = un - deux;
+                    Console.WriteLine("Resultat " + result);
+                    break;
+                case "*":
+                    result = un * deux;
+                    Console.WriteLine("Resultat " + result);
+                    break;
+                case "/":
+                    result = un / deux;
+                    Console.WriteLine("Resultat " + result);
+                    break;
+                default:
+                        Console.WriteLine("erreur.");
+                    break;
+            }
         }
     }
 }
